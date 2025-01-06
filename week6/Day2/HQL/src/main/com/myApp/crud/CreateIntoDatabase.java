0cc1ca75e783db2e5da0b807cfa48480c7ce3aec
HoOnn <148750499+Ho0nn@users.noreply.github.com>
2025-01-06 19:18:22 +0200

    HQL Aggregation Functions

--- week6/Day2/HQL/src/main/com/myApp/crud/CreateIntoDatabase.java
+++ week6/Day2/HQL/src/main/com/myApp/crud/CreateIntoDatabase.java
@@ -6,16 +6,26 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.Date;
+
 public class CreateIntoDatabase {
 
     public static void main(String args[]){
         SessionFactory sessionFactory=new Configuration()
                 .addAnnotatedClass(Product.class)
                 .addAnnotatedClass(User.class)
-                .configure("hibernate.cfg.xml")
+                .configure("hibernate-config.xml")
                 .buildSessionFactory();
 
         Session session=sessionFactory.getCurrentSession();
+        session.beginTransaction();
+        User user=new User("hanin","h123");
+//        User user1=new User("Ahmed","a123");
+        Product product=new Product("laptop","dell",50000,10,"dell", new Date());
+        session.save(user);
+//        session.save(user1);
+        session.save(product);
+        session.getTransaction().commit();
 
     }
 }
--- week6/Day2/HQL/src/main/com/myApp/crud/QueriesIntoDatabase.java
+++ week6/Day2/HQL/src/main/com/myApp/crud/QueriesIntoDatabase.java
@@ -1,4 +1,35 @@
 package main.com.myApp.crud;
 
+import main.com.myApp.model.Product;
+import main.com.myApp.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+
 public class QueriesIntoDatabase {
+    public static void main(String args[]) {
+        SessionFactory sessionFactory = new Configuration()
+                .addAnnotatedClass(Product.class)
+                .addAnnotatedClass(User.class)
+                .configure("hibernate-config.xml")
+                .buildSessionFactory();
+
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+        // aggregation functions
+     List tot= session.createQuery("select sum(quantity) from Product").getResultList();
+     List count= session.createQuery("select count(*) from Product where price>90").getResultList();
+     List maxPrice= session.createQuery("select max(price) from Product").getResultList();
+     List minPrice= session.createQuery("select min(price) from Product").getResultList();
+     List avgPrice= session.createQuery("select avg(price) from Product").getResultList();
+
+     System.out.println(tot);
+     System.out.println(count);
+     System.out.println(maxPrice);
+     System.out.println(minPrice);
+     System.out.println(avgPrice);
+
+    }
 }
